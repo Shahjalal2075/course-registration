cb4a8aea4627031b7bdef37a11c4e48ebc97d9cc
Shahjalal2075 <shahjalal2075@gmail.com>
2023-09-16 02:58:37 +0600

    remaining credit add

--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -3,13 +3,10 @@ import Cart from '../Cart/Cart';
 
 const Carts = ({carts,credits,price}) => {
 
-    const a=5;
-
-
     return (
         <div className="md:w-1/4 lg:w-1/4 px-3">
             <div className="bg-[#fff] rounded-lg p-4">
-                <h2 className="text-[#2F80ED] text-lg font-bold py-3">Credit Hour Remaining 7 hr</h2>
+                <h2 className="text-[#2F80ED] text-lg font-bold py-3">Credit Hour Remaining {20-credits} hr</h2>
                 <hr className="my-3"/>
                 <div className="">
                     <h2 className="text-[#1C1B1B] text-xl font-bold pb-5">Course Name</h2>
